6d88f6449fc7371842a417017f947a6e3c69dcd6
frank <1147296957@qq.com>
2019-07-30 22:21:57 +0800

    change

--- src/components/components.vue
+++ src/components/components.vue
@@ -1,14 +1,13 @@
 <template>
-	<div id="components">
-		<keep-alive exclude="album">
-			<router-view name='components'></router-view>
-		</keep-alive>
-	</div>
+  <div id="components">
+    <keep-alive exclude="album">
+      <router-view name='components'></router-view>
+    </keep-alive>
+  </div>
 </template>
-<script>
-	export default{
-		name:'components',
-	}
+<script>export default {
+  name: 'components',
+}
 </script>
 <style>
 
