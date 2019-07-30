519a3ceee1ba36947386af961b09ad1eb16b4d4c
frankXJC <1147296957@qq.com>
2019-07-30 16:16:42 +0800

    删除目录firstVue

--- firstVue/src/components/copyright/index.js
+++
@@ -1,8 +0,0 @@
-import copyrightrohlin from '@/components/copyright/copyright'
-// import Vue from 'vue'
-
-export default{
-	install:(Vue)=>{
-		Vue.component('copyright-rohlin',copyrightrohlin)
-	}
-}
